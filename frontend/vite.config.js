457767c54cf36702b8502d4dc1d959ad785f6985
Nitin0264 <nc8224140@gmail.com>
2026-09-15 21:42:49 +0530

    git hub page deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
+  base: '/',
 })
+
